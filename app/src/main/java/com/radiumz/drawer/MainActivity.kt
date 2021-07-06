ffcbfc1bcc5089d401fd2f22de725f45166d67f6
RADIUMz-Codes <69766210+theabhikr03@users.noreply.github.com>
2021-07-07 01:41:06 +0530

    added fragments

--- app/src/main/java/com/radiumz/drawer/MainActivity.kt
+++ app/src/main/java/com/radiumz/drawer/MainActivity.kt
@@ -11,11 +11,12 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
-    private lateinit var drawerLayout: DrawerLayout
+    lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,18 +36,23 @@ class MainActivity : AppCompatActivity() {
 
 
         navigationView.setNavigationItemSelectedListener {
+            it.isChecked=true
             when (it.itemId) {
                 R.id.dashboard -> {
                     Toast.makeText(this, "Nav", Toast.LENGTH_SHORT).show()
+                    replaceFragment(Dashboard(),it.title.toString())
                 }
                 R.id.favorites -> {
                     Toast.makeText(this, "Fav", Toast.LENGTH_SHORT).show()
+                    replaceFragment(Favorites(),it.title.toString())
                 }
                 R.id.profile -> {
                     Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                    replaceFragment(Profile(),it.title.toString())
                 }
                 R.id.about -> {
                     Toast.makeText(this, "About", Toast.LENGTH_SHORT).show()
+                    replaceFragment(About(),it.title.toString())
                 }
             }
             return@setNavigationItemSelectedListener true
@@ -59,6 +65,15 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+    private fun replaceFragment(fragment: Fragment,title:String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction =fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.addToBackStack(title)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
     }
 
 }

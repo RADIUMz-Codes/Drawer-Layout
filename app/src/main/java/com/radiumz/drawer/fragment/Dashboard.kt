55ae50dd103249d03db4f14d54f5e7e14fe2b300
RADIUMz <abhi.radiumz@gmail.com>
2021-07-20 03:55:46 +0530

    Added functionality of checking Internet before Calling API

--- app/src/main/java/com/radiumz/drawer/fragment/Dashboard.kt
+++ app/src/main/java/com/radiumz/drawer/fragment/Dashboard.kt
@@ -1,14 +1,18 @@
 package com.radiumz.drawer.fragment
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -110,55 +114,80 @@ class Dashboard : Fragment() {
     want to override the methods of a class with
     very few modifications.
 */
-        val jsonObjectRequest=object : JsonObjectRequest(Request.Method.GET, url, null,
-            Response.Listener {
-                              val success =it.getBoolean("success")
-                if(success){
-                    val data = it.getJSONArray("data")
-                    for(i in 0 until data.length()){
-                        val bookJsonObject= data.getJSONObject(i)
-                        val bookObject = Book(
-                            bookJsonObject.getString("book_id"),
-                            bookJsonObject.getString("name"),
-                            bookJsonObject.getString("author"),
-                            bookJsonObject.getString("rating"),
-                            bookJsonObject.getString("price"),
-                            bookJsonObject.getString("image")
-                        )
-                        bookList.add(bookObject)
-                        recyclerAdapter = DashboardAdapter(activity as Context, bookList)
-
-                        recyclerDashboard.adapter = recyclerAdapter
-
-                        recyclerDashboard.layoutManager = layoutManager
-
-                        recyclerDashboard.addItemDecoration(
-                            DividerItemDecoration(
-                                recyclerDashboard.context,
-                                (layoutManager as LinearLayoutManager).orientation
+        if(ConnectionManager().checkConnectivity(activity as Context))
+        {
+            val jsonObjectRequest=object : JsonObjectRequest(Request.Method.GET, url, null,
+                Response.Listener {
+                    val success =it.getBoolean("success")
+                    if(success){
+                        val data = it.getJSONArray("data")
+                        for(i in 0 until data.length()){
+                            val bookJsonObject= data.getJSONObject(i)
+                            val bookObject = Book(
+                                bookJsonObject.getString("book_id"),
+                                bookJsonObject.getString("name"),
+                                bookJsonObject.getString("author"),
+                                bookJsonObject.getString("rating"),
+                                bookJsonObject.getString("price"),
+                                bookJsonObject.getString("image")
                             )
-                        )
+                            bookList.add(bookObject)
+                            recyclerAdapter = DashboardAdapter(activity as Context, bookList)
+
+                            recyclerDashboard.adapter = recyclerAdapter
+
+                            recyclerDashboard.layoutManager = layoutManager
+
+                            recyclerDashboard.addItemDecoration(
+                                DividerItemDecoration(
+                                    recyclerDashboard.context,
+                                    (layoutManager as LinearLayoutManager).orientation
+                                )
+                            )
+                        }
                     }
-                }
-                else{
-                    Toast.makeText(activity as Context ,"Error",Toast.LENGTH_SHORT).show()
+                    else{
+                        Toast.makeText(activity as Context ,"Error",Toast.LENGTH_SHORT).show()
+                    }
+
+                },Response.ErrorListener {
+                    print("Error is $it")
+                }){
+
+                override fun getHeaders(): MutableMap<String, String> { // this INHERITED class returns mutable maps
+                    // this functions returns HashMaps, there won't be any contradictions as it is built on top of MutableMaps
+                    val headers= HashMap<String,String>()
+                    headers["Content-type"] = "application/json" // 1st key and value
+                    headers["token"]="c5011a8786c3f4" // unique token
+                    return headers
                 }
 
-        },Response.ErrorListener {
-            print("Error is $it")
-        }){
-
-            override fun getHeaders(): MutableMap<String, String> { // this INHERITED class returns mutable maps
-                // this functions returns HashMaps, there won't be any contradictions as it is built on top of MutableMaps
-                val headers= HashMap<String,String>()
-                headers["Content-type"] = "application/json" // 1st key and value
-                headers["token"]="c5011a8786c3f4" // unique token
-                return headers
             }
 
+            queue.add(jsonObjectRequest)
         }
+        else{
+            val dialog= AlertDialog.Builder(activity as Context)
+            dialog.setTitle("Error")
+            dialog.setMessage("Internet Connection is Not Found")
+            dialog.setPositiveButton("Open Setting"){text,listenet->
+                // Open setting if internet is unavailable
+                val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                startActivity(settingsIntent)
 
-        queue.add(jsonObjectRequest)
+                // Used this so that when we return back after changing the settings then fragment restarts
+                activity?.finish()
+            }
+            dialog.setNegativeButton("Exit"){ text,listener->
+                // Close app
+                ActivityCompat.finishAffinity(activity as Activity)
+            }
+//                Create Dialog
+            dialog.create()
+//                Show Dialog
+            dialog.show()
+
+        }
 
         return view
     }

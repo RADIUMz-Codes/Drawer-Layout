f79cb2e914412544649541e8fd35376dd8e1f06a
RADIUMz <abhi.radiumz@gmail.com>
2021-07-21 00:03:13 +0530

    Added Description Activity and Made its UI

--- app/src/main/java/com/radiumz/drawer/activity/DescriptionActivity.kt
+++ app/src/main/java/com/radiumz/drawer/activity/DescriptionActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.radiumz.drawer.R
 
 class DescriptionActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_description)
